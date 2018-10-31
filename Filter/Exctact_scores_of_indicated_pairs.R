9138a3db6e119a0a09d9a06b0454f62378640597
hideki <htanizawa@wistar.org>
2018-10-31 14:14:02 -0400

    BEDファイルのnameカラムを考慮するように変更

--- Filter/Exctact_scores_of_indicated_pairs.R
+++ Filter/Exctact_scores_of_indicated_pairs.R
@@ -3,7 +3,7 @@
 
 suppressPackageStartupMessages(library("optparse"))
 option_list <- list(  
-  make_option(c("-i", "--in"),help="pair information to extract (chr1,start1,end1,chr2,start2,end2)"),
+  make_option(c("-i", "--in"),help="pair information to extract (chr1,start1,end1,name1,chr2,start2,end2,name2)"),
   make_option(c("--dir"), default="NA", help="directory of matrices files"),
   make_option(c("--normalize"), default=FALSE, help="distance normalize (TURE) or not"),
   make_option(c("-o", "--out"),help="output files of scores"),
@@ -27,21 +27,17 @@ if(substring(DIR, nchar(DIR), nchar(DIR)) != "/"){
 suppressWarnings(suppressMessages(library(dplyr)))
 
 D_target <- read.table(FILE_in, header=FALSE, sep="\t", stringsAsFactors = FALSE)
-D_target <- D_target[,1:6]
-colnames(D_target) <- c("chr1", "start1", "end1", "chr2", "start2", "end2")
-D_target <- D_target %>% mutate(chr=chr1, key1=paste(chr1, start1, end1, sep=":"), key2=paste(chr2, start2, end2, sep=":")) %>% select(chr, key1, key2)
+D_target <- D_target[,1:8]
+colnames(D_target) <- c("chr1", "start1", "end1", "name1", "chr2", "start2", "end2", "name2")
+D_target <- D_target %>% mutate(chr=chr1, key1=paste(chr1, start1, end1, sep=":"), key2=paste(chr2, start2, end2, sep=":")) %>% select(chr, key1, key2, name1, name2)
 
 D_score <- c()
 Chromosomes <- unique(D_target %>% pull(chr))
-# options(check.bounds = FALSE)
 for(c in Chromosomes){
   FILE_object <- paste(DIR, c, ".rds", sep="")
   if(file.exists(FILE_object)){
     map <- readRDS(FILE_object)
     r <- rownames(map)
-    # LocList <- strsplit(r, ":")
-    # LocMatrix <- matrix(unlist(LocList), ncol=3, byrow=TRUE)
-    # RESOLUTION <- as.numeric(LocMatrix[1,3]) - as.numeric(LocMatrix[1,2]) + 1
     
     if(as.character(opt["normalize"]) =="TRUE"){
       total_per_line <- apply(map, 1, sum)
@@ -68,12 +64,11 @@ for(c in Chromosomes){
       map <- ifelse(map == 0, 0, log2(map / map_expect))
     }
     
-    D_score_c <- D_target %>% filter(chr==c & key1 %in% r & key2 %in% r) %>% select(-chr)
+    D_score_c <- D_target %>% filter(chr==c & key1 %in% r & key2 %in% r) %>% select(key1, key2) %>% distinct(key1, key2, .keep_all = TRUE)
     
     if(nrow(D_score_c) > 0){
       score <- map[D_score_c %>% as.matrix()]
       D_score_c <- D_score_c %>% mutate(score=score)
-      D_score_c <- D_score_c %>% distinct(key1, key2, .keep_all = TRUE)
       if(is.null(D_score)){
         D_score <- D_score_c
       }else{
@@ -89,7 +84,7 @@ if(is.null(D_score)){
 }else{
   D_target <- dplyr::left_join(D_target, D_score, by=c("key1", "key2"))
   if(as.character(opt["header"]) == "TRUE"){
-    write.table(D_target %>% select(key1, key2, score), FILE_out, row.names = FALSE, col.names = FALSE, quote = FALSE, sep="\t")
+    write.table(D_target %>% select(name1, name2, score), FILE_out, row.names = FALSE, col.names = FALSE, quote = FALSE, sep="\t")
   }else{
     write.table(D_target %>% select(score), FILE_out, row.names = FALSE, col.names = FALSE, quote = FALSE, sep="\t")
   }
