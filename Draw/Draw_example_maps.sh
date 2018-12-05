2a68d3e727e8e8980cccc82fc1c6f1d8b40e0005
hideki <htanizawa@wistar.org>
2018-12-05 17:30:08 -0500

    colorについての設定を修正

--- Draw/Draw_example_maps.sh
+++ Draw/Draw_example_maps.sh
@@ -6,8 +6,8 @@ get_usage(){
 
 Usage : $0 [OPTION] [target sample name(s). separated by space]
 
-Format of location file should have following columns, resolution, color, chr, start, end
-File should be deliminated by tab. Order is flexible but should have header as described in above.
+Format of location file should have following columns, resolution, chr, start, end
+File should be deliminated by tab. Order is flexible and okay to have extrac columns but should have header as described in above.
 
 Description
 	-h, --help
@@ -24,6 +24,9 @@ Description
 
 	-d, --data [data directory]
 		data directory
+
+	-c, --color [color list]
+		specify color for each samples. separated by space but surrounded with double quatation
 EOF
 
 }
@@ -32,8 +35,8 @@ get_version(){
 	echo "${0} version 1.0"
 }
 
-SHORT=hvi:o:
-LONG=help,version,in:,out:
+SHORT=hvi:o:d:c:
+LONG=help,version,in:,out:,data:,color:
 PARSED=`getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@"`
 if [[ $? -ne 0 ]]; then
 	exit 2
@@ -62,6 +65,10 @@ while true; do
 			DIR_DATA="$2"
 			shift 2
 			;;
+		-c|--color)
+			COLORS=($2)
+			shift 2
+			;;
 		--)
 			shift
 			break
@@ -79,6 +86,7 @@ TIME_STAMP=$(date +"%Y-%m-%d")
 [ ! -n "${FILE_location}" ] && echo "Please specify location file" && exit 1
 [ ! -n "${DIR_OUT}" ] && echo "Please specify output directory" && exit 1
 [ ! -n "${DIR_DATA}" ] && echo "Please specify data directory" && exit 1
+[ ! -n "${COLORS}" ] && echo "Please specify colors" && exit 1
 [ $# -lt 1 ] && echo "Please specify target Hi-C sample name(s)" && exit 1
 
 SAMPLES=$@
@@ -98,13 +106,15 @@ file2database.R -i ${FILE_location} --id TRUE --db ${DB_loc} --table loc
 for id in $(sqlite3 ${DB_loc} "select id from loc")
 do
 	RESOLUTION=$(sqlite3 ${DB_loc} "select resolution from loc where id='${id}'")
-	COL=$(sqlite3 ${DB_loc} "select color from loc where id='${id}'")
 	CHR=$(sqlite3 ${DB_loc} "select chr from loc where id='${id}'")
 	START=$(sqlite3 ${DB_loc} "select start from loc where id='${id}'")
 	END=$(sqlite3 ${DB_loc} "select end from loc where id='${id}'")
 
+	i=0
 	for NAME in $SAMPLES
 	do
+		COL=${COLORS[$i]}
+		let i=${i}+1
 		sbatch -n 4 --job-name=${id}_${NAME}_hic $(sq --node) -o "${DIR_OUT}/log/${TIME_STAMP}_map_for_${id}_${CHR}_${START}_${END}.log" --open-mode append --wrap="Rscript --vanilla --slave ${DIR_LIB}/Draw_matrix.R -i ${DIR_DATA}/${NAME}/${RESOLUTION}/ICE/${CHR}.rds --normalize NA --zero NA --na na --chr ${CHR} --start ${START} --end ${END} --unit p --max 0.95 --color $COL --width 800 -o ${DIR_OUT}/img/${id}_${NAME}_hic.png"
 
 		sbatch -n 4 --job-name=${id}_${NAME}_tad $(sq --node) -o "${DIR_OUT}/log/${TIME_STAMP}_tad_for_${id}_${CHR}_${START}_${END}.log" --open-mode append --wrap="Rscript --vanilla --slave ${DIR_LIB}/Draw_borderStrength.R -i ${DIR_DATA}/${NAME}/${RESOLUTION}/ICE/${CHR}.rds --chr ${CHR} --start ${START} --end ${END} --width 800 --height 50 --out ${DIR_OUT}/img/${id}_${NAME}_tad.png"
@@ -124,9 +134,6 @@ done
 #==============================================================
 FILE_md=${DIR_OUT}/${TIME_STAMP}_summary.md
 FILE_html=${DIR_REP}/${TIME_STAMP}_summary.html
-
-sbatch -n 4 --job-name=1 $(sq --node) -o "${DIR_OUT}/log/${TIME_STAMP}_report.log" --open-mode append <<-EOFFF
-#!/bin/sh
 
 cat <<EOF > $FILE_md
 # title of report
@@ -167,11 +174,11 @@ do
 		EOF
 	done
 done
-EOFFF
+
 
 cat<<EOF
 Run following command at computer with pandoc program
-> cd ${DIR_OUT} && pandoc $FILE_md -s --self-contained -t html5 -c ~/.pandoc/github.css  -o $FILE_html
+cd ${DIR_OUT} && pandoc $FILE_md -s --self-contained -t html5 -c ~/.pandoc/github.css  -o $FILE_html
 EOF
 
 
