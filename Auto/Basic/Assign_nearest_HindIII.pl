f836ce266694ddf73f7a837de208603cff82a022
hideki <htanizawa@wistar.org>
2019-03-21 13:58:23 -0400

    restriction fileを外部から受け取るように変更

--- Auto/Basic/Assign_nearest_HindIII.pl
+++ Auto/Basic/Assign_nearest_HindIII.pl
@@ -1,17 +1,5 @@
 #!/usr/bin/perl
-# 2015/06/18 repeatのフラグを新しく作る
-# 2015/04/18 bowtie2用にいくつか修正
-# 2014/11/04 modify for HiC of S.pombe
-# 2014/03/05 changed for human
-# 2013/09/11 update for using latest s.pombe sequence
-# 2012/12/05 Bug fix. Location was differently reported
-# 2012/11/06 Alignment by BWA with several information for HiD or ChIA-PET data
-# 2012/10/27 Alignment by BWA without allowing repeat location for HiD (ChIA-PET) data
-
-####
-#### cation !!!!!!  (aligned location was shift to HindIII location. Not original place)
-####
-
+# Find neareast restriction sites
 
 
 use strict;
@@ -28,55 +16,18 @@ $| = 0;
 #---------------------------------------
 my $unit = 20000;	# section file bin size (20000bp)
 
-if(@ARGV != 8  or $ARGV[0] eq '--help'){
-	die "Usage : $0 -a [sam file 1] -b [sam file 2] -o [output map file] -x [organism]\n";
+if(@ARGV != 12  or $ARGV[0] eq '--help'){
+	die "Usage : $0 -a [sam file 1] -b [sam file 2] -o [output map file] -x [organism] -e [enzyme definition file] -d [enzyme index file]\n";
 }
 
-
 my %opt;
-getopts("a:b:o:x:", \%opt);
+getopts("a:b:o:x:e:d:", \%opt);
 my $FILE_sam1 = $opt{a};
 my $FILE_sam2 = $opt{b};
 my $FILE_map = $opt{o};
 my $ORGANISM = $opt{x};
-
-my $FILE_HINDIII_index;
-my $FILE_HINDIII_def;
-if($ORGANISM eq 'human'){
-	$FILE_HINDIII_index = '/wistar/noma/Data/Human_seq/hg19/Sectioning_MboI.txt';
-	$FILE_HINDIII_def = '/wistar/noma/Data/Human_seq/hg19/MboI_sites.txt';
-}elsif($ORGANISM eq 'human_EBV'){
-	$FILE_HINDIII_index = '/wistar/noma/Data/Human_seq/hg19_EBV/Sectioning_MboI.txt';
-	$FILE_HINDIII_def = '/wistar/noma/Data/Human_seq/hg19_EBV/MboI_sites.txt';
-}elsif($ORGANISM eq 'human_HindIII'){
-	$FILE_HINDIII_index = '/wistar/noma/Data/Human_seq/hg19/Sectioning_HindIII.txt';
-	$FILE_HINDIII_def = '/wistar/noma/Data/Human_seq/hg19/HindIII_sites.txt';
-}elsif($ORGANISM eq 'mouse'){
-	$FILE_HINDIII_index = '/wistar/noma/Data/Mouse_seq/mm10/Sectioning_MboI.txt';
-	$FILE_HINDIII_def = '/wistar/noma/Data/Mouse_seq/mm10/MboI_sites.txt';
-}elsif($ORGANISM eq 'pombe'){
-	$FILE_HINDIII_def = '/wistar/noma/Data/S.Pombe_seq/pombase_ASM294v1.18/MboI_sites.txt';
-	$FILE_HINDIII_index = '/wistar/noma/Data/S.Pombe_seq/pombase_ASM294v1.18/Sectioning_MboI.txt';
-}elsif($ORGANISM eq 'pombe_HindIII'){
-	$FILE_HINDIII_def = '/wistar/noma/Data/S.Pombe_seq/pombase_ASM294v1.18/HindIII_sites.txt';
-	$FILE_HINDIII_index = '/wistar/noma/Data/S.Pombe_seq/pombase_ASM294v1.18/Sectioning_HindIII.txt';
-}else{
-	die "please specify correct organism name\n";
-}
-
-
-#---------------------------------------
-# SAM format
-#---------------------------------------
-#@SQ	SN:chr1	LN:5579133
-#@SQ	SN:chr2	LN:4539804
-#@SQ	SN:chr3	LN:2452883
-#2_ChIA-PET_linkerAA	0	chr2	1896903	37	25M	*	0	0	ATTAGCCAAGCGGTTTCATGATATC	@DDDBABDFHHEEIIIAHI@9<BHB	XT:A:U	NM:i:0	X0:i:1	X1:i:0	XM:i:0	XO:i:0	XG:i:0	MD:Z:25
-#4_ChIA-PET_linkerAA	16	chr3	2789	0	25M	*	0	0	ATATCCCTCCTCTCTCCACCAAAAG	B??::@FDAFBGHFDFDADA?BB?:	XT:A:R	NM:i:0	X0:i:5	X1:i:0	XM:i:0	XO:i:0	XG:i:0	MD:Z:25
-#5_ChIA-PET_linkerAA	16	chr3	1233976	37	25M	*	0	0	TCCTTTCGAGTATCAACCAGCTTCA	?FCA3AC3IC>BBIFFDF<AA++A1	XT:A:U	NM:i:0	X0:i:1	X1:i:0	XM:i:0	XO:i:0	XG:i:0	MD:Z:25
-#6_ChIA-PET_linkerAA	16	chr3	16411	0	25M	*	0	0	AATATTTTTTTTACTAGGATTTGTG	HBF@IIGIIIIGEIFDFHFDDDDA:	XT:A:R	NM:i:0	X0:i:3	X1:i:0	XM:i:0	XO:i:0	XG:i:0	MD:Z:25
-#7_ChIA-PET_linkerAA	0	chr2	1601563	0	25M	*	0	0	TTTCGATGACTTCCCTAATAAATTA	@FFFDFFFGDHGG>FHIEEHIIII@	XT:A:R	NM:i:0	X0:i:2	X1:i:0	XM:i:0	XO:i:0	XG:i:0	MD:Z:25	XA:Z:chr2,+1646901,25M,0;
-
+my $FILE_ENZYME_index = $opt{e};
+my $FILE_ENZYME_def = $opt{d};
 
 #---------------------------------------
 # map format
@@ -96,12 +47,12 @@ if($ORGANISM eq 'human'){
 # 13. hindiii location 2
 
 #---------------------------------------
-# read Hind III file
+# read restriction definition file
 #---------------------------------------
-### read Hind III sites
-my %Hinds;
+### read def sites
+my %Enzymes;
 {
-	my $fh_in = IO::File->new($FILE_HINDIII_def) or die "cannot open $FILE_HINDIII_def: $!";
+	my $fh_in = IO::File->new($FILE_ENZYME_def) or die "cannot open $FILE_ENZYME_def: $!";
 	while($_ = $fh_in->getline()){
 		if(m/^#/){
 			next;
@@ -109,14 +60,14 @@ my %Hinds;
 		s/\r?\n//;
 		my ($number, $chr, $pos, $before, $after) = split /\t/;
 		my $id = $chr . ':' . $number;
-		$Hinds{$id} = $pos;
+		$Enzymes{$id} = $pos;
 	}
 	$fh_in->close();
 }
-### read Hind III index file
+### read index file
 my %Hind_index;
 {
-	my $fh_in = IO::File->new($FILE_HINDIII_index) or die "cannot open $FILE_HINDIII_index: $!";
+	my $fh_in = IO::File->new($FILE_ENZYME_index) or die "cannot open $FILE_ENZYME_index: $!";
 	while($_ = $fh_in->getline()){
 		if(m/^#/){
 			next;
@@ -146,18 +97,10 @@ my $fh_sam2 = IO::File->new($FILE_sam2) or die "cannot open $FILE_sam2: $!";
 my $fh_map = IO::File->new($FILE_map, 'w') or die "cannot write $FILE_map: $!";
 while(my $sam1 = $fh_sam1->getline()){
 	my $sam2 = $fh_sam2->getline();
-#	if($sam1 =~ m/^\@/){
-#		next;
-#	}
-#	$sam1 =~ s/\r?\n//;
-#	$sam2 =~ s/\r?\n//;
+	$sam1 =~ s/\r?\n//;
+	$sam2 =~ s/\r?\n//;
 	my ($id1, $flag1, $chr1, $position1, $mapQ1, $CIAGR1, $mate_a1, $mate_b1, $mate_c1, $seq1, $quality1, @option1) = split /\t/, $sam1;
 	my ($id2, $flag2, $chr2, $position2, $mapQ2, $CIAGR2, $mate_a2, $mate_b2, $mate_c2, $seq2, $quality2, @option2) = split /\t/, $sam2;
-
-
-	$id1 = substr($id1, 0, -2);
-	$id2 = substr($id2, 0, -2);
-
 
 	if($id1 ne $id2){
 		die ("$id1 and $id2 is different\n");
@@ -256,14 +199,14 @@ sub FindHindIIIinfo{
 		my $MIN_left_id = '';
 		my $MIN_right_id = '';
 		foreach my $i(@candidates){
-			if($pos < $Hinds{$i}){
-				my $distance = $Hinds{$i} - $pos;
+			if($pos < $Enzymes{$i}){
+				my $distance = $Enzymes{$i} - $pos;
 				if($distance < $MIN_left){
 					$MIN_left = $distance;
 					$MIN_left_id = $i;
 				}
 			}else{
-				my $distance = $pos - $Hinds{$i};
+				my $distance = $pos - $Enzymes{$i};
 				if($distance < $MIN_right){
 					$MIN_right = $distance;
 					$MIN_right_id = $i;
@@ -290,10 +233,11 @@ sub FindHindIIIinfo{
 			$hinID .= 'R';
 		}
 
-		return ($hinID, $Hinds{$MIN_id});
+		return ($hinID, $Enzymes{$MIN_id});
 	}else{
 #		warn ("Nearest restriction cites were not found for $chr : $pos \n");
 		return ('NA', 'NA');
-	}}
+	}
+}
 
 
