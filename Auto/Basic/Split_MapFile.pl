87b6f1c3cbde89f065b608e0dfaca0a382d8bb57
hideki <htanizawa@wistar.org>
2019-03-25 17:36:51 -0400

    生物種の違いをパラメータで受けるように修正

--- Auto/Basic/Split_MapFile.pl
+++ Auto/Basic/Split_MapFile.pl
@@ -9,18 +9,9 @@ use File::Basename;
 use Carp qw(croak);
 $| = 0;
 
-
-
 if(@ARGV != 6 or $ARGV[0] eq '--help'){
 	die "Usage : $0 -i [map file] -l [chromosome length] -o [file list]\n";
 }
-
-my %chromosome_length = (
-	'human' => 3095677412,
-	'human_EBV' => 3157782322,
-	'mouse' => 2725537669,
-	'pombe' => 12571820,
-);
 
 my %opt;
 getopts("i:l:o:", \%opt);
@@ -50,7 +41,6 @@ while($_ = $fh_map->getline()){
 		next;
 	}
 
-
 	# 右と左を比べて小さい方を左にするように統一する
 	if($chr1 eq $chr2){
 		if($loc1 > $loc2){
@@ -62,30 +52,16 @@ while($_ = $fh_map->getline()){
 			($uniq2, $uniq1) = ($uniq1, $uniq2);
 		}
 	}else{
-		if($TARGET_ORGANISM eq 'pombe'){
-			my $comparison = $chr1 cmp $chr2;
-			if($comparison == 1){
-				($chr2, $chr1) = ($chr1, $chr2);
-				($loc2, $loc1) = ($loc1, $loc2);
-				($direction2, $direction1) = ($direction1, $direction2);
-				($mapQ2, $mapQ1) = ($mapQ1, $mapQ2);
-				($resID2, $resID1) = ($resID1, $resID2);
-				($resLoc2, $resLoc1) = ($resLoc1, $resLoc2);
-				($uniq2, $uniq1) = ($uniq1, $uniq2);
-			}
-		}else{
-			if(&ComparisonChr($chr1, $chr2) == 1){
-				($chr2, $chr1) = ($chr1, $chr2);
-				($loc2, $loc1) = ($loc1, $loc2);
-				($direction2, $direction1) = ($direction1, $direction2);
-				($mapQ2, $mapQ1) = ($mapQ1, $mapQ2);
-				($resID2, $resID1) = ($resID1, $resID2);
-				($resLoc2, $resLoc1) = ($resLoc1, $resLoc2);
-				($uniq2, $uniq1) = ($uniq1, $uniq2);
-			}
+		if(&ComparisonChr($chr1, $chr2) == 1){
+			($chr2, $chr1) = ($chr1, $chr2);
+			($loc2, $loc1) = ($loc1, $loc2);
+			($direction2, $direction1) = ($direction1, $direction2);
+			($mapQ2, $mapQ1) = ($mapQ1, $mapQ2);
+			($resID2, $resID1) = ($resID1, $resID2);
+			($resLoc2, $resLoc1) = ($resLoc1, $resLoc2);
+			($uniq2, $uniq1) = ($uniq1, $uniq2);
 		}
 	}
-
 
 	my $bin = int($loc1 / $RESOLUTION);
 
@@ -103,49 +79,46 @@ while($_ = $fh_map->getline()){
 $fh_map->close();
 
 
+sub Chr2Num{
+	my ($chr) = @_;
+	my $num = $chr;
+	if($chr =~ m/chr(\w+)/){
+		$num = $1;
+	}
+	if($num eq 'X'){
+		$num = 23;
+	}
+	if($num eq 'Y'){
+		$num = 24;
+	}
+	if($num eq 'M'){
+		$num = 25;
+	}
+	if($num eq 'EBV'){
+		$num = 26;
+	}
+	if($num eq 'I'){
+		$num = 1;
+	}
+	if($num eq 'II'){
+		$num = 2;
+	}
+	if($num eq 'III'){
+		$num = 3;
+	}
+	return $num;
+}
+
 sub ComparisonChr{
 	my ($chr1, $chr2) = @_;
-	my ($num1, $num2) = ($chr1, $chr2);
-	if($chr1 =~ m/chr(\w+)/){
-		$num1 = $1;
-	}
-	if($chr2 =~ m/chr(\w+)/){
-		$num2 = $1;
-	}
-	if($num1 eq 'X'){
-		$num1 = 23;
-	}
-	if($num2 eq 'X'){
-		$num2 = 23;
-	}
-	if($num1 eq 'Y'){
-		$num1 = 24;
-	}
-	if($num2 eq 'Y'){
-		$num2 = 24;
-	}
-	if($num1 eq 'M'){
-		$num1 = 25;
-	}
-	if($num2 eq 'M'){
-		$num2 = 25;
-	}
-	if($num1 eq 'EBV'){
-		$num1 = 26;
-	}
-	if($num2 eq 'EBV'){
-		$num2 = 26;
-	}
-
+	my $num1 = &Chr2Num($chr1);
+	my $num2 = &Chr2Num($chr2);
 	if($num1 < $num2){
 		return -1;
 	}else{
 		return 1;
 	}
 }
-
-
-
 
 #---------------------------------------
 # ファイルのリストを作成する &&
