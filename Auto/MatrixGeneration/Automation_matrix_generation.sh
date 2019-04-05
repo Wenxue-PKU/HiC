50740846f0cf35e6b4e22986becb02484716dde6
hideki <htanizawa@wistar.org>
2019-04-05 15:58:56 -0400

    Black listを使わないオプションを追加

--- Auto/MatrixGeneration/Automation_matrix_generation.sh
+++ Auto/MatrixGeneration/Automation_matrix_generation.sh
@@ -5,59 +5,63 @@
 # コマンド引数の受取り
 #==============================================================
 SHORT=d:n:x:r:o:t:e:c:
-LONG=directory:,name:,organism:,resolution:,log:,intra:,normalization:,raw:
+LONG=directory:,name:,organism:,resolution:,log:,intra:,normalization:,raw:,use_blacklist:
 PARSED=`getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@"`
 if [[ $? -ne 0 ]]; then
-    exit 2
+	exit 2
 fi
 eval set -- "$PARSED"
 
 while true; do
-    case "$1" in
-        -d|--directory)
-            DIR_DATA="$2"
-            shift 2
-            ;;
-        -n|--name)
-            NAME="$2"
-            shift 2
-            ;;
-        -x|--organism)
-            ORGANISM="$2"
-            shift 2
-            ;;
-        -r|--resolution)
-            RESOLUTION_string="$2"
-            RESOLUTION=${RESOLUTION_string/kb/000}
-            shift 2
-            ;;
-        -o|--log)
-            FILE_LOG="$2"
-            shift 2
-            ;;
-        -t|--intra)
-        	# if only intra chromosome TRUE, otherwise FALSE (default TRUE)
-            FLAG_INTRA="$2"
-            shift 2
-            ;;
-        -e|--normalization)
-            FLAG_NORM="$2"
-            shift 2
-            ;;
-        -c|--raw)
-        	# whether making raw matrices or not
-            FLAG_RAW="$2"
-            shift 2
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            echo "Programming error"
-            exit 3
-            ;;
-    esac
+	case "$1" in
+		-d|--directory)
+			DIR_DATA="$2"
+			shift 2
+			;;
+		-n|--name)
+			NAME="$2"
+			shift 2
+			;;
+		-x|--organism)
+			ORGANISM="$2"
+			shift 2
+			;;
+		-r|--resolution)
+			RESOLUTION_string="$2"
+			RESOLUTION=${RESOLUTION_string/kb/000}
+			shift 2
+			;;
+		-o|--log)
+			FILE_LOG="$2"
+			shift 2
+			;;
+		-t|--intra)
+			# if only intra chromosome TRUE, otherwise FALSE (default TRUE)
+			FLAG_INTRA="$2"
+			shift 2
+			;;
+		-e|--normalization)
+			FLAG_NORM="$2"
+			shift 2
+			;;
+		-c|--raw)
+			# whether making raw matrices or not
+			FLAG_RAW="$2"
+			shift 2
+			;;
+		--use_blacklist)
+			FLAG_blacklist="$2"
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Programming error"
+			exit 3
+			;;
+	esac
 done
 
 [ ! -n "${NAME}" ] && echo "Please specify NAME" && exit 1
@@ -68,29 +72,26 @@ done
 FLAG_INTRA=${FLAG_INTRA:-TRUE}
 FLAG_NORM=${FLAG_NORM:-TRUE}
 FLAG_RAW=${FLAG_RAW:-TRUE}
+FLAG_blacklist=${FLAG_blacklist:-TRUE}
 
 TIME_STAMP=$(date +"%Y-%m-%d")
 DIR_LIB=$(dirname $0)
 
 case $ORGANISM in
-	pombe) DIR_contig=/wistar/noma/Data/S.Pombe_seq/pombase_ASM294v2.30
-		   CHRs=(I II III)
-	       LENGTH=(5579133 4539804 2452883) ;;
-	human) DIR_contig=/wistar/noma/Data/Human_seq/hg19
-		   #CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY)
-	       #LENGTH=(249250621 243199373 198022430 191154276 180915260 171115067 159138663 146364022 141213431 135534747 135006516 133851895 115169878 107349540 102531392 90354753 81195210 78077248 59128983 63025520 48129895 51304566 155270560 59373566) ;;
-		   CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX)
-	       LENGTH=(249250621 243199373 198022430 191154276 180915260 171115067 159138663 146364022 141213431 135534747 135006516 133851895 115169878 107349540 102531392 90354753 81195210 78077248 59128983 63025520 48129895 51304566 155270560) ;;
-	human_EBV) DIR_contig=/wistar/noma/Data/Human_seq/hg19_EBV
-		   CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY EBV)
-	       LENGTH=(249250621 243199373 198022430 191154276 180915260 171115067 159138663 146364022 141213431 135534747 135006516 133851895 115169878 107349540 102531392 90354753 81195210 78077248 59128983 63025520 48129895 51304566 155270560 59373566 171823) ;;
-	mouse) DIR_contig=/wistar/noma/Data/Mouse_seq/mm10
-		   #CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY)
-	       #LENGTH=(195471971 182113224 160039680 156508116 151834684 149736546 145441459 129401213 124595110 130694993 122082543 120129022 120421639 124902244 104043685 98207768 94987271 90702639 61431566 171031299 91744698) ;;
-		   CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX)
-	       LENGTH=(195471971 182113224 160039680 156508116 151834684 149736546 145441459 129401213 124595110 130694993 122082543 120129022 120421639 124902244 104043685 98207768 94987271 90702639 61431566 171031299) ;;
-	*)     echo "Please specify correct organism"
-	       eixt 1 ;;
+	pombe)	CHRs=(I II III)
+			LENGTH=(5579133 4539804 2452883) ;;
+	human)	#CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY)
+			#LENGTH=(249250621 243199373 198022430 191154276 180915260 171115067 159138663 146364022 141213431 135534747 135006516 133851895 115169878 107349540 102531392 90354753 81195210 78077248 59128983 63025520 48129895 51304566 155270560 59373566) ;;
+			CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX)
+			LENGTH=(249250621 243199373 198022430 191154276 180915260 171115067 159138663 146364022 141213431 135534747 135006516 133851895 115169878 107349540 102531392 90354753 81195210 78077248 59128983 63025520 48129895 51304566 155270560) ;;
+	human_EBV)	CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY EBV)
+			LENGTH=(249250621 243199373 198022430 191154276 180915260 171115067 159138663 146364022 141213431 135534747 135006516 133851895 115169878 107349540 102531392 90354753 81195210 78077248 59128983 63025520 48129895 51304566 155270560 59373566 171823) ;;
+	mouse) #CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY)
+			#LENGTH=(195471971 182113224 160039680 156508116 151834684 149736546 145441459 129401213 124595110 130694993 122082543 120129022 120421639 124902244 104043685 98207768 94987271 90702639 61431566 171031299 91744698) ;;
+			CHRs=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX)
+			LENGTH=(195471971 182113224 160039680 156508116 151834684 149736546 145441459 129401213 124595110 130694993 122082543 120129022 120421639 124902244 104043685 98207768 94987271 90702639 61431566 171031299) ;;
+	*)	echo "Please specify correct organism"
+		eixt 1 ;;
 esac
 
 
@@ -127,7 +128,11 @@ if [ $FLAG_RAW = "TRUE" ]; then
 	JOB_ID=($(squeue -o "%j %F" -u htanizawa | grep -e "badFragment_${NAME}" | cut -f2 -d' ' | xargs))
 	JOB_ID_string=$(IFS=:; echo "${JOB_ID[*]}")
 	DEPEND=""; [ -n "$JOB_ID_string" ] && DEPEND="--dependency=afterok:${JOB_ID_string}"
-	sbatch -N 1 -n 5 --exclusive=user --job-name=RAW_${NAME}_${RESOLUTION_string} $DEPEND -o ${FILE_LOG} --open-mode append --wrap="cd ${DIR_DATA}; perl $PRO_RAW_matrix -i ${NAME}_fragment.db -o ${NAME}/${RESOLUTION_string}/Raw/  -r ${RESOLUTION} -b ${NAME}_bad_fragment.txt"
+	if [ "$FLAG_blacklist" = "TRUE" ]; then
+		sbatch -N 1 -n 5 --exclusive=user --job-name=RAW_${NAME}_${RESOLUTION_string} $DEPEND -o ${FILE_LOG} --open-mode append --wrap="cd ${DIR_DATA}; perl $PRO_RAW_matrix -i ${NAME}_fragment.db -o ${NAME}/${RESOLUTION_string}/Raw/  -r ${RESOLUTION} -b ${NAME}_bad_fragment.txt"
+	else
+		sbatch -N 1 -n 5 --exclusive=user --job-name=RAW_${NAME}_${RESOLUTION_string} $DEPEND -o ${FILE_LOG} --open-mode append --wrap="cd ${DIR_DATA}; perl $PRO_RAW_matrix -i ${NAME}_fragment.db -o ${NAME}/${RESOLUTION_string}/Raw/  -r ${RESOLUTION}"
+	fi
 
 
 	JOB_ID=($(squeue -o "%j %F" -u htanizawa | grep -e "RAW_${NAME}_${RESOLUTION_string}" | cut -f2 -d' ' | xargs))
