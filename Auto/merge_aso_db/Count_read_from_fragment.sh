f82b6d36bdd10f42cd665951e330c448b95464ae
hideki <htanizawa@wistar.org>
2019-05-31 13:09:37 -0400

    Fix : if database not found, exit

--- Auto/merge_aso_db/Count_read_from_fragment.sh
+++ Auto/merge_aso_db/Count_read_from_fragment.sh
@@ -2,6 +2,8 @@
 
 cd ${DIR_DATA}
 
+[ ! -e ${SAMPLE}_fragment.db ] && echo "${SAMPLE}_fragment.db was not found" && exit 1
+
 echo -n "intra-chromosome: "
 sqlite3 ${SAMPLE}_fragment.db "select sum(score) from fragment where chr1 = chr2;"
 
