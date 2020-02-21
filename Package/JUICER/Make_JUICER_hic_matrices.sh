a7f01da314282c762dc5fab53c957ea1a273f611
hideki <rafysta@gmail.com>
2020-02-21 14:55:32 -0800

    Add: convert matrices to JUICER format

---
+++ Package/JUICER/Make_JUICER_hic_matrices.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# Prepare Juicer's hic matrices
+
+get_usage(){
+	cat <<EOF
+
+Usage : $0 [OPTION]
+
+Description
+	-h, --help
+		show help
+
+	-i, --in [map.gz file]
+		NAME.map.gz file
+
+	-o, --out [output hic file]
+		sample name
+
+	-r, --resolution [resolution list]
+		resolution list separated by ,. For example 5000,10000,20000,100000
+	
+	-q, --mapQ [mapQ threshold 30 (default)]
+		MapQ threshold. Defulat is 30.
+
+	-x, --ref [ex. hg19]
+		organism reference sequence name
+EOF
+
+}
+
+get_version(){
+	echo "${0} version 1.0"
+}
+
+SHORT=hi:o:r:q:x:
+LONG=help,in:,out:,resolution:,mapQ:,ref:
+PARSED=`getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@"`
+if [[ $? -ne 0 ]]; then
+	exit 2
+fi
+eval set -- "$PARSED"
+
+while true; do
+	case "$1" in
+		-h|--help)
+			get_usage
+			exit 1
+			;;
+		-i|--in)
+			FILE_MAP="$2"
+			shift 2
+			;;
+		-o|--out)
+			FILE_OUT="$2"
+			shift 2
+			;;
+		-r|--resolution)
+			RESOLUTION="$2"
+			shift 2
+			;;
+		-q|--mapQ)
+			THRESHOLD_MAPQ="$2"
+			shift 2
+			;;
+		-x|--ref)
+			REF="$2"
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Programming error"
+			exit 3
+			;;
+	esac
+done
+
+DIR_LIB=$(dirname $0)
+TIME_STAMP=$(date +"%Y-%m-%d")
+
+[ ! -n "${FILE_MAP}" ] && echo "Please specify map file" && exit 1
+[ ! -n "${FILE_OUT}" ] && echo "Please specify output hic file name" && exit 1
+[ ! -n "${RESOLUTION}" ] && echo "Please specify resolution" && exit 1
+[ ! -n "$REF{}" ] && echo "Please specify ref" && exit 1
+THRESHOLD_MAPQ=${CHR_include:-30}
+
+# wget http://hicfiles.s3.amazonaws.com/internal/juicebox_tools/8.5.16/juicebox_tools.jar
+PROGRAM_JUICER=${HOME}/Software/juicebox_tools.jar
+
+[ ! -e $PROGRAM_JUICER ] && echo "juicer program not found" && exit 1
+
+
+#-----------------------------------------------
+# Load setting
+#-----------------------------------------------
+source ${DIR_LIB}/../../utils/load_setting.sh -x $REF -r NA
+
+### Create Temporary directory
+DIR_tmp=$(mktemp -d /tmp/tmp_hic_juicer.XXXXXX)
+FILE_data=${DIR_tmp}/data_for_hic.txt
+FILE_hic=${DIR_tmp}/matrix.list
+trap "rm -r ${DIR_tmp}" 0
+
+
+#==============================================================
+# Convert matrices
+#==============================================================
+zcat ${FILE_MAP} | awk 'NR>1 && $8=="U" && $15=="U"{
+	if($4=="+"){str1=0}else{str1=1}
+	if($11=="+"){str2=0}else{str2=1}
+	print $1,str1,$2,$3,$6,str2,$9,$10,$13,$5,$12
+}' | sort -k3,3d -k7,7d > ${FILE_data}
+
+java -Xmx8g -jar $PROGRAM_JUICER pre -r ${RESOLUTION} -q $THRESHOLD_MAPQ -t ${DIR_tmp} ${FILE_data} ${FILE_hic} $CHROM_SIZE
+mv $FILE_hic ${FILE_OUT}
+
+exit 0
+
+
+
